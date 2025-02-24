1057d0dfa8da844f2df3c9b3884498b2a25003d8
Bot-350 <cbabot350@gmail.com>
2025-02-24 19:36:30 -0400

    first commit

---
+++ practica1.py
@@ -0,0 +1,38 @@
+
+m = 'hola'
+e =  ' '
+c = 'Estructura de Datos'
+
+m1 = 'Estoy'
+m2 = 'en'
+m3 = '3er'
+m4 = 'Semestre'
+
+m5 = 'Mi'
+m6 = 'curso'
+m7 = 'es'
+m8 = 'el'
+m9 = '205'
+print(m + e + c)
+print(m + e + m1 + e + m2 + e + m3 + e + m4)
+print(m5 + e +m6 + e + m7 + e + m8 + e + m9)
+
+
+suma1 =30 + 70
+suma1 =str(suma1)
+print('el resultado de la suma es:'+ (suma1))
+
+suma2 = 100 + 75
+#suma2 = str(suma2)
+print(f'el resultado de la suma es: {suma2}')
+
+multi = 25 * 35
+print(f'el resultado de la multiplicacion es: {multi}')
+
+mensajed = "hola curso"
+extraer_sub = mensajed.find('o')
+print(extraer_sub)
+
+mensajec= 'hola estoy en 3er semestre'
+extraer_sub2 = mensajed.find('3')
+print(extraer_sub2)
